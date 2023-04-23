793bf983def44f1827df68afb22e474f29c5943a
romanaduraciova <98467853+romanaduraciova@users.noreply.github.com>
2023-04-23 16:55:15 +0200

    Initial commit of files for website

---
+++ script.js
@@ -0,0 +1,17 @@
+const mobileLines = document.querySelector(".mobile_lines");
+const navList = document.querySelector(".nav_list");
+const navItem = document.querySelectorAll(".nav_item");
+
+mobileLines.addEventListener("click", showMenu);
+navItem.forEach(item => item.addEventListener("click", hideMenu));
+window.addEventListener("scroll", hideMenu);
+
+function showMenu() {
+    mobileLines.classList.toggle("active");
+    navList.classList.toggle("active");
+}
+
+function hideMenu() {
+    mobileLines.classList.remove("active");
+    navList.classList.remove("active");
+}
