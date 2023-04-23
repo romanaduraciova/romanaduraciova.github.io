6fc48d51eb7cbb2f2e9a64dd7117d7558680ae17
romanaduraciova <98467853+romanaduraciova@users.noreply.github.com>
2023-04-23 23:34:17 +0200

    added comments to the source files

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ const mobileLines = document.querySelector(".mobile_lines");
 const navList = document.querySelector(".nav_list");
 const navItem = document.querySelectorAll(".nav_item");
 
+// Show menu when clicking
 mobileLines.addEventListener("click", showMenu);
+// Hide menu after clicking on one of the sections
 navItem.forEach(item => item.addEventListener("click", hideMenu));
+
+// Hide menu when scrolling
 window.addEventListener("scroll", hideMenu);
 
+// Function for showing menu
 function showMenu() {
     mobileLines.classList.toggle("active");
     navList.classList.toggle("active");
 }
 
+// Function for hiding menu
 function hideMenu() {
     mobileLines.classList.remove("active");
     navList.classList.remove("active");
